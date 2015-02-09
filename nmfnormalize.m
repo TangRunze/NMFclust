e52dcc3d8e093d6acc529119a35ee23b390ae774
Runze Tang <tangrunze@gmail.com>
2015-02-08 22:11:02 -0500

    Add optimization options.

--- nmfnormalize.m
+++ nmfnormalize.m
@@ -13,29 +13,32 @@ hInit = hInit/diag(sum(hInit));
 % Combine W and H
 whInit = [reshape(wInit, n*(n-1)*r, 1); reshape(hInit, r*T, 1)];
 
-% Construct inequality linear constraint
-ineConMat = zeros(r+T, length(whInit));
+% Construct equality linear constraint
+eConMat = zeros(r+T, length(whInit));
 
 % Constraints for W
 for i = 1:r
-    ineConMat(i, ((i-1)*n*(n-1)+1):(i*n*(n-1))) = 1;
+    eConMat(i, ((i-1)*n*(n-1)+1):(i*n*(n-1))) = 1;
 end
 
 % Constraints for H
 for i = 1:T
-    ineConMat(i+r, (n*(n-1)*r+(i-1)*r+1):(n*(n-1)*r+i*r)) = 1;
+    eConMat(i+r, (n*(n-1)*r+(i-1)*r+1):(n*(n-1)*r+i*r)) = 1;
 end
 
-ineConVec = ones(r+T, 1);
+eConVec = ones(r+T, 1);
 
 %% Solve Optimization Problem
 
 % projectoptions = optimoptions('fmincon', 'TolX', 1e-6, 'MaxIter', 10000,...
 %     'MaxFunEvals', 10000);
 
-whHat = fmincon(@(wh) nmfnormobjfun(X, wh, n, r, T, lambda), whInit, ...
-    [], [], ineConMat, ineConVec, zeros(length(whInit), 1), ...
-    Inf(length(whInit), 1));
+options = optimoptions('fmincon', 'TolX', 1e-6, ...
+    'MaxIter', 10000, 'MaxFunEvals', 10000);
+
+[whHat] = fmincon(@(wh) nmfnormobjfun(X, wh, n, r, T, lambda), ...
+    whInit, [], [], eConMat, eConVec, zeros(length(whInit), 1), ...
+    Inf(length(whInit), 1), [], options);
 
 wHat = whHat(1:(n*(n-1)*r), 1);
 hHat = whHat((n*(n-1)*r+1):end, 1);
